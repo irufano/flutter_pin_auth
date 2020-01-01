963f560c4a631cacae7b602e4981bbddce87b630
M. Irfan Hidayat <irfanhidayatms@outlook.com>
2020-01-01 18:28:16 +0700

    init

---
+++ lib/views/pin_auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PinAuth extends StatefulWidget {
+  @override
+  _PinAuthState createState() => _PinAuthState();
+}
+
+class _PinAuthState extends State<PinAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
